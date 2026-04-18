35c92098e49efeed39f50f4cba075bc2018931fc
fefiy <foziayimam87@gmail.com>
2026-04-18 11:34:19 +0300

    fix: add end point for creating end point for adding and updating category

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import {
   addCategory,
-  getCategoriesHierarchy
+  getCategoriesHierarchy,
+  updateCategory,
 } from "../controller/categoryController.js";
 
 const router = Router();
 
-router.post("/create",addCategory)
-router.get("/",getCategoriesHierarchy)
-
+router.post("/create", addCategory);
+router.get("/", getCategoriesHierarchy);
+router.put("/:id", updateCategory);
 
 export default router;
